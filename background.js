ffeb620b874db24ecfe29bcff94a5d1616904abf
locnguyen1842 <locnguyen1842@gmail.com>
2021-02-21 17:19:40 +0700

    improve code

--- background.js
+++ background.js
@@ -1,18 +1,42 @@
-let sotsMenuItem = {
+let extensionMenu = {
     id: 'sots',
     contexts: ["selection"],
-    title: 'Search on This site'
+    title: 'Search on This site',
+}
+const resourceSite = "https://raw.githubusercontent.com/locnguyen1842/SoTs/master/resources/sites.json"
+
+chrome.contextMenus.create(extensionMenu);
+
+chrome.runtime.onInstalled.addListener(() => {
+    initSites()
+});
+
+const initSites = async () => {
+    chrome.storage.local.set({sotsSites: await getResource(resourceSite), cachedAt: Date.now()})
 }
 
-chrome.contextMenus.create(sotsMenuItem);
-
 chrome.contextMenus.onClicked.addListener(function(info,tab) {
-    
-    if(info.menuItemId === 'sots' && info.selectionText) {
-        chrome.tabs.sendMessage(tab.id,{"clickedSots": true, "selectedText": info.selectionText},function(res) {
-            if(res.openNewTab) {
-                chrome.tabs.create({"url": res.url, "index": tab.index + 1});
-            }
+    chrome.tabs.sendMessage(tab.id, {event: "onClickedSots"}, res => {
+        chrome.storage.local.get(['sotsSites'], result => {
+            const url = result.sotsSites?.[res.hostname?.replace('www.', '')]
+            handleContextMenuClick(info, tab, url)
         })
-    }
+    })
 })
+
+const handleContextMenuClick = (info, tab, url) => {
+    url ? searchOnAvailableSite(url, info.selectionText, ++tab.index) : searchOnDefaultProvider(info.selectionText, "NEW_TAB")
+}
+
+const getResource = async (resource) => {
+    const response = await fetch(resource)
+    return await response.json()
+}
+
+const searchOnAvailableSite = (url, text, tabIndex) => {
+    chrome.tabs.create({"url": url.replace(/%s/g, text), "index": tabIndex})
+}
+
+const searchOnDefaultProvider = (text, disposition) => {
+    chrome.search.query({disposition: disposition, text: text})
+}
--- content.js
+++ content.js
@@ -1,46 +1,6 @@
 
-function getSiteInfo () {
-    
-    let popularSite = [
-        {hostname: 'youtube.com', searchPath: 'results', searchParam: 'searchQuery'},
-        {hostname: 'facebook.com', searchPath: 'search', searchParam: 'q'},
-        {hostname: 'instagram.com', searchPath: 'explore/tags', searchParam: ''},
-        {hostname: 'stackoverflow.com', searchPath: 'search', searchParam: 'q'},
-    ];
-    let currentHostname = handleHostname(window.location.hostname);
-
-    let siteObj = popularSite.find( site => site.hostname === currentHostname);
-
-    if(siteObj !== undefined) {
-        siteObj.isPopularSite = true;
-        return siteObj;
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if(request.event === "onClickedSots") {
+        sendResponse({"hostname": window.location.hostname})
     }
-    return false;
-}
-
-function popularSiteSotsGenerator(siteObj,selectedText = 'sots') {
-    let siteParam = siteObj.searchParam !== '' ? `?${siteObj.searchParam}=${selectedText}` : `/${selectedText}`;
-    return `http://${siteObj.hostname}/${siteObj.searchPath}${siteParam}`
-}
-
-function handleHostname(hostname) {
-    splittedHostname = hostname.split('.');
-    if(splittedHostname[0] === 'www') {
-        splittedHostname.shift()
-    }
-    return splittedHostname.join('.')
-}
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if(request.clickedSots) {
-            let sots = `http://www.google.com/search?q=${request.selectedText}`;
-            let siteObj = getSiteInfo();
-            if(siteObj.isPopularSite) {
-                sots = popularSiteSotsGenerator(siteObj,request.selectedText);
-            }
-            
-            sendResponse({"openNewTab": true, "url": sots})
-        }
-    }
-);
+});
